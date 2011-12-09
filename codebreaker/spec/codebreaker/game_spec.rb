0b763ca869ca4b8f4d84862d4da6a3755cbf55bf
Leandro Moreira <leandro.rhcp99@gmail.com>
2011-12-09 21:52:30 -0200

    the second refactoring, using the let method

--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -4,21 +4,18 @@ module Codebreaker
 
  describe Game do
   describe "#start" do
+    let(:output) { double('output').as_null_object }
+    let(:game) { Game.new(output) }
 
-   before(:each) do
-    @output = double('output').as_null_object
-    @game = Game.new(@output)
-   end
+    it "sends a welcome message" do
+     output.should_receive(:puts).with('Welcome to Codebreaker!')
+     game.start
+    end
 
-   it "sends a welcome message" do
-    @output.should_receive(:puts).with('Welcome to Codebreaker!')
-    @game.start
-   end
-
-   it "prompts for the first guess" do
-    @output.should_receive(:puts).with('Enter guess:')
-    @game.start
-   end
+    it "prompts for the first guess" do
+     output.should_receive(:puts).with('Enter guess:')
+     game.start
+    end
 
   end
  end
