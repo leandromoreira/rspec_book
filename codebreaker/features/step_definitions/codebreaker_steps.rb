b186f106dfb85f6370f2b19a1c9f0bed468b10a9
Leandro Moreira <leandro.rhcp99@gmail.com>
2011-12-09 00:39:56 -0200

    creating the code to our definitions

--- codebreaker/features/step_definitions/codebreaker_steps.rb
+++ codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -2,6 +2,7 @@ Given /^I am not yet playing$/ do
 end
 
 When /^I start a new game$/ do
+ Codebreaker::Game.new.start
 end
 
 Then /^I should see "([^"]*)"$/ do |arg1|
---
+++ codebreaker/lib/codebreaker/game.rb
@@ -0,0 +1,8 @@
+module Codebreaker
+
+ class Game
+  def start
+  end
+ end
+
+end
