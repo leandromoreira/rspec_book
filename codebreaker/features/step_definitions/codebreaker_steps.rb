ed81d823d0833eee97a7740cd3315e3abfdd0d55
Leandro Moreira <leandro.rhcp99@gmail.com>
2011-12-09 00:27:37 -0200

    now with steps, even blank but passing all the tests

---
+++ codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -0,0 +1,17 @@
+Given /^I am not yet playing$/ do
+end
+
+When /^I start a new game$/ do
+end
+
+Then /^I should see "([^"]*)"$/ do |arg1|
+end
+
+Given /^the secret code is "([^"]*)"$/ do |arg1|
+end
+
+When /^I guess "([^"]*)"$/ do |arg1|
+end
+
+Then /^the mark should be "([^"]*)"$/ do |arg1|
+end
