afc2bb7811df7b7e13bd0d94aa95bc1635c4891e
Leandro Moreira <leandro.rhcp99@gmail.com>
2012-01-01 15:45:27 -0200

    another example of how far we can go with conciseness of rspec

--- hello/spec/my_spec.rb
+++ hello/spec/my_spec.rb
@@ -3,6 +3,9 @@ class Magazine
  def initialize
   @pages = (1..100).to_a
  end
+ def published?
+  true
+ end
 end
 
 describe "An amazing rspec solution to tests" do
@@ -10,4 +13,10 @@ describe "An amazing rspec solution to tests" do
   linux = Magazine.new
   linux.should have(100).pages
  end
+end
+
+#this is CONSICE
+
+describe Magazine do
+ it { should be_published }
 end
