009e6d14c83675c37ebb137b4d2d1b0b674068ba
Leandro Moreira <leandro.rhcp99@gmail.com>
2012-01-22 16:04:36 -0200

    showtime app

---
+++ showtime/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showtime::Application.config.secret_token = 'de1b57113b7bc97a7c1076872dde878a96ed6cbc4340eb2507e70cd03ff39b298de9bdf517dddf2c42f0534c7bcb22c51dad67c59103b56db5f788a8d67fb8dd'
